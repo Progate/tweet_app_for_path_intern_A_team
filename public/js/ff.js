8e6016e59fa2eb589f952669c5610ffab2aeab7a
4equest <107108812+4equest@users.noreply.github.com>
2023-08-20 03:27:06 +0900

    add フォローフォロワーの取得と表示

--- public/js/ff.js
+++ public/js/ff.js
@@ -30,9 +30,47 @@ followersItem.addEventListener('click', () => {
         followingItem.classList.add('selectable-item');
         followingItem.classList.remove('selectable-item-selected');
 
-        //ここにapiとの通信を行って要素を追加するjs
+        const userID = '3';
+
+        fetch(`/api/followers/${userID}`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(user => {
+                    const div = document.createElement('div');
+                    div.className = 'users-index-item';
+
+                    const userLeft = document.createElement('div');
+                    userLeft.className = 'user-left';
+                    const userIcon = document.createElement('img');
+                    userIcon.className = 'user-left';
+                    userIcon.src = user.iconURL;
+                    userLeft.appendChild(userIcon);
+                    div.appendChild(userLeft);
+
+                    const userRight = document.createElement('div');
+                    userRight.className = 'user-right';
+                    const userLink = document.createElement('a');
+                    userLink.href = '/users/' + user.userID;
+                    userLink.setAttribute('data-test', 'user-item-link');
+                    userLink.textContent = user.userName;
+                    userRight.appendChild(userLink);
+                    div.appendChild(userRight);
+
+                    //ここはユーザーのフォロー状況に合わせて変える
+                    const followButton = document.createElement('div');
+                    followButton.className = 'follo-follower-list-button';
+                    const followButtonInner = document.createElement('div');
+                    followButtonInner.className = 'follow-button';
+                    followButtonInner.textContent = 'Follow';
+                    followButton.appendChild(followButtonInner);
+                    div.appendChild(followButton);
+
+                    document.querySelector('.follo-follower-list').appendChild(div);
+                });
+            });
     }
 });
+
 
 followingItem.addEventListener('click', () => {
     if (!followingItem.classList.contains('selectable-item-selected')) {
@@ -41,6 +79,42 @@ followingItem.addEventListener('click', () => {
         followersItem.classList.add('selectable-item');
         followersItem.classList.remove('selectable-item-selected');
 
-        //ここにapiとの通信を行って要素を追加するjs
+        const userID = '3';
+
+        fetch(`/api/followers/${userID}`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(user => {
+                    const div = document.createElement('div');
+                    div.className = 'users-index-item';
+
+                    const userLeft = document.createElement('div');
+                    userLeft.className = 'user-left';
+                    const userIcon = document.createElement('img');
+                    userIcon.className = 'user-left';
+                    userIcon.src = user.iconURL;
+                    userLeft.appendChild(userIcon);
+                    div.appendChild(userLeft);
+
+                    const userRight = document.createElement('div');
+                    userRight.className = 'user-right';
+                    const userLink = document.createElement('a');
+                    userLink.href = '/users/' + user.userID;
+                    userLink.setAttribute('data-test', 'user-item-link');
+                    userLink.textContent = user.userName;
+                    userRight.appendChild(userLink);
+                    div.appendChild(userRight);
+
+                    const followButton = document.createElement('div');
+                    followButton.className = 'follo-follower-list-button';
+                    const followButtonInner = document.createElement('div');
+                    followButtonInner.className = 'following-button'; //押さえたら変えるようにしよう
+                    followButtonInner.textContent = 'Following';
+                    followButton.appendChild(followButtonInner);
+                    div.appendChild(followButton);
+
+                    document.querySelector('.follo-follower-list').appendChild(div);
+                });
+            });
     }
 });
