def38ff67cbc0cd0ffd4459f6898d0b899b1a344
ckybb <kiiyamans.k138@gmail.com>
2023-08-28 20:29:02 +0900

    異常系を追加しました

--- src/routes/follow.ts
+++ src/routes/follow.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import {PrismaClient} from "@prisma/client";
+import {getUser} from "@/models/user";
+import {databaseManager} from "@/db/index";
 
 const prisma = new PrismaClient();
 export const followRouter = express.Router();
@@ -7,6 +9,7 @@ export const followRouter = express.Router();
 followRouter.post("/:userId", async (req, res) => {
   const {userId} = req.params;
   const currentUserId = req.authentication?.currentUserId;
+  const userNumber = Number(userId);
 
   if (currentUserId === undefined) {
     // `ensureAuthUser` enforces `currentUserId` is not undefined.
@@ -15,7 +18,31 @@ followRouter.post("/:userId", async (req, res) => {
   }
 
   if (userId === String(currentUserId)) {
-    return res.status(400).send("You can't follow yourself.");
+    return res.status(400).send("Invalid error: You can't follow yourself.");
+  }
+  // userIdがnumberじゃない場合
+  // 存在しないuserIdの場合
+  if (isNaN(userNumber)) {
+    return res.status(400).send("Invalid error: userId is not number.")
+  }
+
+  const user = await getUser(userNumber)
+  console.log(user)
+
+  if (user === null) {
+    return res.status(400).send("Invalid error: target user does not exist.")
+  }
+  // 既にフォローしている場合
+  const prisma = databaseManager.getInstance();
+  const follow = await prisma.follow.findFirst({
+    where: {
+      followingId: currentUserId,
+      followedId: userNumber,
+    },
+  });
+
+  if (follow !== null) {
+    return res.status(400).send("Invalid error: You have already followed target user.")
   }
 
   await prisma.follow.create({
