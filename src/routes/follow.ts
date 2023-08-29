63b10ae71f9fd113b27a93631d8b618280cf19ea
ckybb <kiiyamans.k138@gmail.com>
2023-08-29 16:55:28 +0900

    follow/unfollow機能のAPI側を実装

--- src/routes/follow.ts
+++ src/routes/follow.ts
@@ -3,7 +3,6 @@ import {PrismaClient} from "@prisma/client";
 import {getUser} from "@/models/user";
 import {databaseManager} from "@/db/index";
 
-const prisma = new PrismaClient();
 export const followRouter = express.Router();
 
 followRouter.post("/:userId", async (req, res) => {
@@ -27,8 +26,7 @@ followRouter.post("/:userId", async (req, res) => {
   }
 
   const user = await getUser(userNumber)
-  console.log(user)
-
+  
   if (user === null) {
     return res.status(400).send("Invalid error: target user does not exist.")
   }
@@ -52,5 +50,56 @@ followRouter.post("/:userId", async (req, res) => {
     },
   });
 
-  res.send("Followed successfully");
+  res.json({ success: "true" });
 });
+
+followRouter.delete("/:userId", async (req, res) => {
+  const {userId} = req.params;
+  const currentUserId = req.authentication?.currentUserId;
+  const userNumber = Number(userId);
+
+  if (currentUserId === undefined) {
+    // `ensureAuthUser` enforces `currentUserId` is not undefined.
+    // This must not happen.
+    return res.status(400).send("Invalid error: currentUserId is undefined.");
+  }
+
+  if (userId === String(currentUserId)) {
+    return res.status(400).send("Invalid error: You can't unfollow yourself.");
+  }
+  // userIdがnumberじゃない場合
+  // 存在しないuserIdの場合
+  if (isNaN(userNumber)) {
+    return res.status(400).send("Invalid error: userId is not number.")
+  }
+
+  const user = await getUser(userNumber)
+  
+  if (user === null) {
+    return res.status(400).send("Invalid error: target user does not exist.")
+  }
+  // フォローしていない場合
+  const prisma = databaseManager.getInstance();
+  const follow = await prisma.follow.findFirst({
+    where: {
+      followingId: currentUserId,
+      followedId: userNumber,
+    },
+  });
+
+  if (follow === null) {
+    return res.status(400).send("Invalid error: You haven't followed target user yet.")
+  }
+
+  await prisma.follow.delete({
+    where: {
+      followingId_followedId: {
+        followingId: currentUserId,
+        followedId: Number(userId),
+      },
+    },
+  });
+
+  res.json({ success: "true" });
+});
+
